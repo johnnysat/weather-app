3ab2a3ea89b9134e486c442381df2deaa12214a4
Johnny <johnny_s.o@hotmail.com>
2022-11-20 15:22:21 -0300

    Adicionando const que chama dados da API weather

---
+++ script.js
@@ -0,0 +1,12 @@
+const api = {
+  key: '9420c587001998ce8f388bc3675e0525',
+  base: 'https://api.openweathermap.org/data/2.5/',
+  units: metric,
+  lang: pt_br,
+}
+
+const city = document.querySelector('.city_name');
+const temp = document.querySelector('.city_temp');
+const input = document.querySelector('.search_button');
+
+
